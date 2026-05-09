b515cd6829b75b54b9a565067444b832572e3be3
SatrioAtallaR <satrioatallar51@gmail.com>
2026-05-09 09:32:42 +0700

    Proteus dan Code Project

---
+++ Arduino_Code/FINPRO8.ino.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+#line 1 "C:\\Users\\satri\\OneDrive\\文档\\Arduino\\FINPRO8\\FINPRO8.ino"
+#line 1 "C:\\Users\\satri\\OneDrive\\文档\\Arduino\\FINPRO8\\FINPRO8.ino"
+void setup();
+#line 6 "C:\\Users\\satri\\OneDrive\\文档\\Arduino\\FINPRO8\\FINPRO8.ino"
+void loop();
+#line 1 "C:\\Users\\satri\\OneDrive\\文档\\Arduino\\FINPRO8\\FINPRO8.ino"
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
